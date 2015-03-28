28d1676b937a099ab3ac3e39e0eb8e968417d336
Praveen kumar <praveen.k9894@gmail.com>
2015-03-29 00:32:41 +0530

    Create tm-get-stops.php

---
+++ tm-get-stops.php
@@ -0,0 +1,33 @@
+<div class="control-group">
+    <label for="stopnumber" class="control-label">Stop Number</label>
+    <div class="controls">
+
+        <select name="stopnumber" id="select2" class='input-large'>
+            <?php
+            require_once "praveenlib.php";
+            require_once "datas.php";
+
+            $dbconnection = connectSQL($dbdetails);
+
+            if(mysqli_connect_errno()) //Check if any error occurred on connection
+            {
+                echo "db_connection_fail";
+            }
+            else
+            {
+                $sql="select * from tm_bus_stop where route=".$_POST['route'];
+
+                $result=mysqli_query($dbconnection,$sql);
+
+                while($row=mysqli_fetch_array($result))
+                {
+                    echo '<option value="'.$row['id'].'">'.$row['id']." (".$row['name'].')</option>';
+
+                }
+            }
+            ?>
+
+
+        </select>
+    </div>
+</div>
